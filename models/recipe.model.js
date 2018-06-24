7385b607063fcdf57f9c76a0bbf3227c14f3213e
sberlinches <sergioberlinches@gmail.com>
2018-06-24 14:25:14 -0700

    Format changes

--- models/recipe.model.js
+++ models/recipe.model.js
@@ -14,36 +14,11 @@ class Recipe {
 }
 
 let recipes = [
-    new Recipe(
-        0,
-        'Pizza Margherita',
-        [
-            new Item('Pizza crust', 1),
-            new Item('Tomato sauce', 1),
-            new Item('Mozzarella', 8),
-            new Item('Plum tomatoes', 2),
-            new Item('Basil', 1)
-        ],
-        [
-            'Put everything together',
-            'Bake for 14-16 minutes'
-        ],
-        60
-    ),
-    new Recipe(
-        1,
-        'Raw chicken',
-        [
-            new Item('Chicken', 1),
-            new Item('Hammer', 1),
-        ],
-        [
-            "Hit the head's chicken with the hammer",
-            "Skin the chicken",
-            "Serve fresh",
-        ],
-        5
-    )
+    new Recipe(0, 'Pizza Margherita', [new Item('Pizza crust', 1), new Item('Tomato sauce', 1),
+        new Item('Mozzarella', 8), new Item('Plum tomatoes', 2), new Item('Basil', 1)], ['Put everything together',
+        'Bake for 14-16 minutes'], 60),
+    new Recipe(1, 'Raw chicken', [new Item('Chicken', 1), new Item('Hammer', 1),], [
+        "Hit the head's chicken with the hammer", "Skin the chicken", "Serve fresh",], 5)
 ];
 
 /**
